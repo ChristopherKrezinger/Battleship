3031dc75f59e870a624c6e2fec810100f7ec5d3c
ChristopherKrezinger <christophertiborkrezinger@yahoo.de>
2024-10-14 21:06:02 +0200

    Delete Illegal_Argument_exception.java

--- Illegal_Argument_exception.java
+++
@@ -1,8 +0,0 @@
-package Battleship_1_0;
-
-//to create custom exceptions
-public class Illegal_Argument_exception extends RuntimeException {
-    public Illegal_Argument_exception(String message) {
-        super(message);
-    }
-}
